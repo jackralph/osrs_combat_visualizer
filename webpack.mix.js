ffaab94c01debd0d1970f8115f83d5b18c0e8862
Jack Ralph <jackralph@Jacks-MBP.local>
2021-03-10 19:31:35 +0000

    [feature] util function working in component

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/components/App.jsx', 'public/js/components/App.js')
-    .react()
+    .copy('resources/js/utils', 'public/js/utils')
+    .js('resources/js/components/App.jsx', 'public/js/components/App.js').react()
     .sass('resources/sass/app.scss', 'public/css');
