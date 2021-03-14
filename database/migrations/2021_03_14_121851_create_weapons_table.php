9e505d467d322b69e5c424700ef5065911e6d8d2
Jack Ralph <jackralph@Jacks-MBP.local>
2021-03-14 14:27:59 +0000

    [feature] json files for equipment/prayers & db migrations for equipment

---
+++ database/migrations/2021_03_14_121851_create_weapons_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeaponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weapons', function (Blueprint $table) {
+            $table->integer('id')->unique();
+            $table->string('name');
+            $table->string('last_updated');
+            $table->boolean('incomplete');
+            $table->boolean('members');
+            $table->boolean('tradeable');
+            $table->boolean('tradeable_on_ge');
+            $table->boolean('stackable');
+            $table->boolean('stacked')->nullable();
+            $table->boolean('noted');
+            $table->boolean('noteable');
+            $table->boolean('linked_id_item')->nullable();
+            $table->boolean('linked_id_noted')->nullable();
+            $table->integer('linked_id_placeholder')->nullable();
+            $table->boolean('placeholder');
+            $table->boolean('equipable');
+            $table->boolean('equipable_by_player');
+            $table->boolean('equipable_weapon');
+            $table->integer('cost');
+            $table->integer('lowalch')->nullable();
+            $table->integer('highalch')->nullable();
+            $table->float('weight')->nullable();
+            $table->integer('buy_limit')->nullable();
+            $table->boolean('quest_item');
+            $table->string("release_date")->nullable();
+            $table->boolean("duplicate");
+            $table->string("examine")->nullable();
+            $table->string("icon");
+            $table->string("wiki_name")->nullable();
+            $table->string("wiki_url")->nullable();
+            $table->text("equipment")->nullable();
+            $table->text("weapon")->nullable();
+            $table->boolean("two_handed")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weapons');
+    }
+}
