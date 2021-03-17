ff9bc5b3fa3d13470f7187c05cd2f5fa95a23f47
Jack Ralph <jackralph@Jacks-MBP.local>
2021-03-17 19:24:32 +0000

    [feature] styling tab icons

--- resources/js/components/interface/elements/InventoryTabs.jsx
+++ resources/js/components/interface/elements/InventoryTabs.jsx
@@ -1,33 +1,34 @@
+import { chunk } from 'lodash';
 import React from 'react';
 
 const InventoryTabs = ({ setCurrentTab }) => {
 
-    const handleTabClick = ({ target: { name } }) => {
-        setCurrentTab(name);
+    const handleTabClick = ({ target: { dataset : { tabName } } }) => {
+        setCurrentTab(tabName);
     }
 
     return (
-        <div className="row text-center mb-3">
-            <div className="col p-1">
-                <img src="/images/Combat_icon.png" name="combat_styles" onClick={handleTabClick} alt="Combat Icon"/>
+        <div className="row mb-3 interface">
+            <div className="col p-0 interface__tab-container" data-tab-name="combat_styles" onClick={handleTabClick}>
+                <img src="/images/Combat_icon.png" alt="Combat Icon"/>
             </div>
-            <div className="col p-1">
-                <img src="/images/Skills_icon.png" name="levels" onClick={handleTabClick} alt="Skills Icon"/>
+            <div className="col p-0 interface__tab-container" data-tab-name="levels" onClick={handleTabClick}>
+                <img src="/images/Skills_icon.png" alt="Skills Icon"/>
             </div>
-            <div className="col p-1">
-                <img src="/images/Monster_examine_icon.png" name="monsters" onClick={handleTabClick} alt="Monster Examine Icon"/>
+            <div className="col p-0 interface__tab-container" data-tab-name="monsters" onClick={handleTabClick}>
+                <img src="/images/Monster_examine_icon.png" alt="Monster Examine Icon"/>
             </div>
-            <div className="col p-1">
-                <img src="/images/Inventory_icon.png" name="inventory" onClick={handleTabClick} alt="Inventory Icon"/>
+            <div className="col p-0 interface__tab-container" data-tab-name="inventory" onClick={handleTabClick}>
+                <img src="/images/Inventory_icon.png" alt="Inventory Icon"/>
             </div>
-            <div className="col p-1">
-                <img src="/images/Worn_equipment_icon.png" name="equipment" onClick={handleTabClick} alt="Worn Equipment Icon"/>
+            <div className="col p-0 interface__tab-container" data-tab-name="equipment" onClick={handleTabClick}>
+                <img src="/images/Worn_equipment_icon.png" alt="Worn Equipment Icon"/>
             </div>
-            <div className="col p-1">
-                <img src="/images/Prayer_tab_icon.png" name="prayers" onClick={handleTabClick} alt="Prayer Tab Icon"/>
+            <div className="col p-0 interface__tab-container" data-tab-name="prayers" onClick={handleTabClick}>
+                <img src="/images/Prayer_tab_icon.png" alt="Prayer Tab Icon"/>
             </div>
-            <div className="col p-1">
-                <img src="/images/Spellbook_icon.png" name="spellbook" onClick={handleTabClick} alt="Spellbook Icon"/>
+            <div className="col p-0 interface__tab-container" data-tab-name="spellbook" onClick={handleTabClick}>
+                <img src="/images/Spellbook_icon.png" alt="Spellbook Icon"/>
             </div>
         </div>
     );
