a3db86ddfb7849c36af37bdb2d5ded496158f416
Jack Ralph <jackralph@Jacks-MBP.local>
2021-03-21 12:18:39 +0000

    [data] seeded monsters / modified potion migartions and seeder

--- database/migrations/2021_03_21_112549_create_monsters_table.php
+++ database/migrations/2021_03_21_112549_create_monsters_table.php
@@ -17,21 +17,16 @@ class CreateMonstersTable extends Migration
             $table->increments('id');
             $table->integer('monster_id');
             $table->integer('combat_level');
-            $table->integer('hit_points');
+            $table->integer('hitpoints')->nullable();
             $table->text('attributes');
             $table->text('category');
-            $table->boolean('duplicate');
-            $table->text('icon')->nullable();
             $table->string('wiki_name');
             $table->string('wiki_url');
             $table->integer('attack_level');
             $table->integer('strength_level');
             $table->integer('defence_level');
             $table->integer('magic_level');
-            $table->integer('range_level');
-            $table->integer('attack_stab');
-            $table->integer('attack_slash');
-            $table->integer('attack_crush');
+            $table->integer('ranged_level');
             $table->integer('attack_magic');
             $table->integer('attack_ranged');
             $table->integer('defence_stab');
@@ -39,10 +34,6 @@ class CreateMonstersTable extends Migration
             $table->integer('defence_crush');
             $table->integer('defence_magic');
             $table->integer('defence_ranged');
-            $table->integer('attack_accuracy');
-            $table->integer('melee_strength');
-            $table->integer('ranged_strength');
-            $table->integer('magic_damage');
         });
     }
 
